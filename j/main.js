ffcca8c10312ec751100f72b1e8bc26a58bda69e
Troy <tbennett@dvc.edu>
2023-11-28 20:55:22 -0800

    added async branch. updated  to use async/await

--- j/main.js
+++ j/main.js
@@ -1,33 +1,84 @@
 
-//This code does NOT create any global variables.
-//Promises can be chained together, with the previous promise
-// passing its results to the next one in the chain.
-// the format is: fetch().then().then().catch()
-//it's easier to read if we put each step in its own line,
-//that's why the periods start the then lines.
+//This code does NOT create any global variables
 
-fetch("houses.json")
-    .then((response) => response.json())
-    .then((data) => {
+/*
+
+fetch('houses.json')
+.then(response => response.json())
+.then(data => {
+    //create a temp holder to append all the html generated inside the forEach iterator
+    let html = '';
+
+    //the argument "house" passed to the arrow function
+    //holds each item in the array in turn. 
+    data.forEach(house => {
+        let family = house.members.join(' | ');
+        
+        // generate the html snippet for one array item
+        //to be added to the "html" temp holder.
+        let objInfo = 
+        `<p class="house">${house.name}</p>
+        <p class="folks">${family}</p>`
+        html += objInfo;
+    });
+
+    //make a reference to the html container where
+    //the info will be displayed.
+    const container = document.querySelector('#container');
+    container.innerHTML = html;
+})
+.catch( err => console.log('Oops!', err));
+//this only runs if there is an error during the above process
+
+*/
+
+
+
+// This is an alternative for the above code using asyn/await 
+//functions. This does create global variables.
+ 
+async function fetchData(api) {
+    try {
+        let response = await fetch(api);
+        let data = await response.json();
+        return data;
+    } catch(err) {
+         //this only runs if an error occurs in above process
+        console.log('Oops!', err);
+    }
+}
+ 
+async function renderData(url, htmlContainer) {
+    try {
+        let data = await fetchData(url);
         //create a temp holder to append all the html generated inside the forEach iterator
-        let html = "";
-
+        let html = '';
+ 
         //the argument "house" passed to the arrow function
-        //holds each item in the array in turn.
-        data.forEach((house) => {
-            let family = house.members.join(" | ");
+        //holds each item in the array in turn. 
+        data.forEach(item => {
 
+            // convert the members array into a string
+            let family = item.members.join(' | ');
+            
             // generate the html snippet for one array item
             //to be added to the "html" temp holder.
-            let objInfo = `<p class="house">${house.name}</p>
-        <p class="folks">${family}</p>`;
+            let objInfo = 
+            `<p class="house">${item.name}</p>
+            <p class="folks">${family}</p>`;
             html += objInfo;
         });
 
         //make a reference to the html container where
         //the info will be displayed.
-        const container = document.querySelector("#container");
+        const container = document.querySelector(htmlContainer);
         container.innerHTML = html;
-    })
-    .catch((err) => console.log("Oops!", err));
-    //this only runs if there is an error during the above process
+
+    } catch(err) {
+        //this only runs if an error occurs in above process
+        console.log('Oops!', err)
+    }
+    
+}
+
+renderData('houses.json', '#container');
